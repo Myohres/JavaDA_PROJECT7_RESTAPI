aa2157cbce67d82f43b072ab7ce63b3b3bcd964a
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-06 03:44:33 +0200

    mapping ratting from DB

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,24 @@
 package com.nnk.springboot.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short id;
+    private String moodysRating;
+    private String sandPRating;
+    private String fitchRating;
+    private short orderNumber;
 }
