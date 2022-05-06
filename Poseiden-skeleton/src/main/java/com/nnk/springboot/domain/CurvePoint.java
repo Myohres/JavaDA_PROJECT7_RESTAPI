5eb8e22fda608702286de5e4118ac07875ed4fc9
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-06 03:41:41 +0200

    mapping curvepoint from DB

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,5 +1,7 @@
 package com.nnk.springboot.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -9,7 +11,17 @@ import java.sql.Timestamp;
 
 
 @Entity
+@Getter
+@Setter
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short id;
+    private short CurveId;
+    private Timestamp asOfDate;
+    private double term;
+    private double value;
+    private Timestamp creationDate;
 }
